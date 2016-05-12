43bc293b968c674d743ec6249569b5f49f82db74
Stanislas Chevallier <contact@syan.me>
2016-05-12 13:26:20 +0200

    updated gitignore

---
+++ SaneScanner/SYMediaPreviewCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SYMediaPreviewCollectionViewCell.h
+//  SaneScanner
+//
+//  Created by Stan Chevallier on 03/05/2016.
+//  Copyright © 2016 Syan. All rights reserved.
+//
+
+#import <MHVideoPhotoGallery/MHVideoPhotoGallery.h>
+
+@interface SYMediaPreviewCollectionViewCell : MHMediaPreviewCollectionViewCell
+
+@end
---
+++ SaneScanner/SYMediaPreviewCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  SYMediaPreviewCollectionViewCell.m
+//  SaneScanner
+//
+//  Created by Stan Chevallier on 03/05/2016.
+//  Copyright © 2016 Syan. All rights reserved.
+//
+
+#import "SYMediaPreviewCollectionViewCell.h"
+
+@implementation SYMediaPreviewCollectionViewCell
+
+@end
